5afa5c50f91ea241797312877257eaca3e1fd31b
David Simoes <das.simoes@outlook.com>
2018-05-30 22:31:23 -0300

    teste user

--- fundamentos/objetos.js
+++ fundamentos/objetos.js
@@ -12,14 +12,14 @@ const prod2 =
 {
     nome: 'Camisa Polo',
     preco: 79.90,
-    obj: 
-    {
-        blabla: 1,
-        obj: 
-        {
-            blablabla: 2
-        }
-    }
+    // obj: 
+    // {
+    //     blabla: 1,
+    //     obj: 
+    //     {
+    //         blablabla: 2
+    //     }
+    // }
 }
 
 console.log(prod2);
