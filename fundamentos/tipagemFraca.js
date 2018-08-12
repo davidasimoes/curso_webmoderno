45e84447f71294e68949dc13209af100c752a4ca
David Simoes <das.simoes@outlook.com>
2018-08-12 12:26:49 -0300

    Tipagem Fraca

---
+++ fundamentos/tipagemFraca.js
@@ -0,0 +1,10 @@
+//JS é uma linguagem de tipagem fraca (dinâmica), onde uma mesma variável pode
+//assumir diversos tipos de dados.
+
+let qualquer = 'legal' //string
+console.log(typeof qualquer) //imprime tipo do dado
+
+qualquer = 3.1516
+console.log(qualquer)
+console.log(typeof qualquer)
+
