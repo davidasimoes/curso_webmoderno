59c952be52b2047ad51defff3a4467dad2438384
David Simoes <das.simoes@outlook.com>
2018-08-11 21:15:44 -0300

    Organização do código JS

--- fundamentos/organizacao.js
+++ fundamentos/organizacao.js
@@ -2,3 +2,16 @@
 
 //Sentença de código
 console.log("Senteça de código")
+
+console.log( //esta é uma sentença com mais de 1 linha de código
+    "Sentença com mais de 1 linha")//esta é uma linha de código
+
+
+//Blocos de código contém { }
+//Isto é um bloco de código
+
+{
+    {
+        console.log("Bloco de código")
+    }
+}
