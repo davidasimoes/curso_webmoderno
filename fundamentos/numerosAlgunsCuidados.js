00be93763d310fedd965852b6a002f28a7f70b7f
David Simoes <das.simoes@outlook.com>
2018-08-12 19:55:19 -0300

    Alguns cuidados com Numbers

---
+++ fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,9 @@
+console.log(7 / 0) //retorna um tipo infinito
+
+//JS verifica se o valor dentro da String pode ser dividido 
+console.log("10" / 2)
+console.log("Show!" * 2) //Não funciona
+console.log(0.1 + 0.7) //Alerta de imprecisão, por conta da especificação de ponto flutuante
+// console.log(4.1 + 3.7 ) 
+// console.log(10.toString()) //Não funciona
+console.log((10).toString) //Funciona
