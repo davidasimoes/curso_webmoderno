a137ae0a2771070b433251e86c3fc63c21171667
David Simoes <das.simoes@outlook.com>
2018-08-11 21:41:22 -0300

    Variáveis e Constantes

---
+++ fundamentos/variaveisEConstantes.js
@@ -0,0 +1,17 @@
+//Atribuição de valor em uma variável com var
+var a = 3
+//Criação de variável com let
+
+//REGRA GERAL: USE LET PARA CRIAR VARIÁVEIS
+let b = 4
+
+var a = 30
+
+//Não redeclarar variáveis let
+b = 40
+
+console.log(a,b)
+
+//Declaração de constantes
+const c = 5
+console.log(a,b,c)
