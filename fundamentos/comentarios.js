a2d95c807b002750ac15b2ac4608f6908e548b42
David Simoes <das.simoes@outlook.com>
2018-08-11 21:24:16 -0300

    Comentários em JS

---
+++ fundamentos/comentarios.js
@@ -0,0 +1,23 @@
+//Comentário de uma linha
+console.log('Linha 1')
+
+/*
+    Comentários
+    de
+    mais
+    de
+    uma
+    linha
+*/
+
+console.log('Linha 2')
+
+/*
+ * Comentário
+ * de múltiplas
+ * linhas
+ * mais 
+ * elegante 
+*/
+
+console.log('Linha 3')
