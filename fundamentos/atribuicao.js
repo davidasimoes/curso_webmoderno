36b5b45c301739f7549539ba6c57f0ecc0d653cc
David Simões <das.simoes@outlook.com>
2018-08-21 11:38:08 -0300

    Atribuição

---
+++ fundamentos/atribuicao.js
@@ -0,0 +1,18 @@
+const a = 7
+let b = 3
+
+b += a //b = b + a
+console.log(b)
+
+b -= 4
+console.log(b) //b = b - 4
+
+b *= 2 //b = b * 2
+console.log(b)
+
+b /= 2 //b = b/2
+console.log(b)
+
+b %= 2 // b = b % 2 resto da divisão por 2
+console.log(b)
+
